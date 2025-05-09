9d7d2d7cb53a473df71c641c2632c3faaed0004e
Temporary User <temp@example.com>
2025-05-09 09:57:24 -0300

    Forçando atualização do VS Code

---
+++ start-git.bat
@@ -0,0 +1,30 @@
+@echo off
+cls
+echo === Iniciando Git ===
+echo.
+
+git init
+
+echo.
+echo === Configurando usuario ===
+echo.
+set /p nome=Digite seu nome: 
+git config --global user.name "%nome%"
+
+set /p email=Digite seu email: 
+git config --global user.email "%email%"
+
+echo.
+echo === Adicionando arquivos ===
+echo.
+git add .
+
+echo.
+echo === Criando primeiro commit ===
+echo.
+git commit -m "Primeiro commit"
+
+echo.
+echo === Git inicializado com sucesso! ===
+echo.
+pause 
