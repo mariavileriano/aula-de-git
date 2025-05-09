9e49a86c81ff9409e0cf5b31e6c3055deb97fbd8
mariavileriano <mariaeduardasilva11032006@gmail.com>
2025-05-09 12:06:40 -0300

    adicionei a palavra maria

--- css/js/index.js
+++ css/js/index.js
@@ -2,6 +2,7 @@
 function somar(a, b) {
     return a + b;
 }
+(maria)
 
 // Exportando a função para ser usada em outros arquivos
 module.exports = somar;
--- start-git.bat
+++ start-git.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo off (maria)
 cls
 echo === Iniciando Git ===
 echo.
